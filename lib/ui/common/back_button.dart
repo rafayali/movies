d86be7f585382d3e3b3c63e714e9b7f2fe13f2d4
Rafay Ali <rufi1233@gmail.com>
2026-01-02 16:22:37 +0500

    chore: remove elevation from back button

--- lib/ui/common/back_button.dart
+++ lib/ui/common/back_button.dart
@@ -1,21 +1,15 @@
 import 'package:flutter/material.dart';
 
 class MoviesBackButton extends StatelessWidget {
-  const MoviesBackButton({
-    super.key,
-    required this.onPressed,
-  });
+  const MoviesBackButton({super.key, required this.onPressed});
 
   final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return FilledButton.tonal(
       onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        shape: const CircleBorder(),
-        elevation: 4,
-      ),
+      style: ElevatedButton.styleFrom(shape: const CircleBorder()),
       child: const Icon(Icons.arrow_back),
     );
   }
